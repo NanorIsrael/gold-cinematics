1991b6c0cd23858c28b9e48702a75ec09be51d60
nanorisrael <israel.nanor@compozelabs.com>
2024-05-14 01:53:00 +0000

    Add html for page one

---
+++ golde-cinematics/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <section>
+      <h1>Elevate your storytelling with golde cinematics</h1>
+      <p>From captivating visuals to immersive naratives, we specialize in bringing
+        your ideas to life on the silver screen let us elevate your vision with our passion,
+        creativity, and dedication to excellence.
+      </p>
+      </section>
+      <section>
+        <img src={"logo512.png"} alt="partners"/>
+
+        {/* <img src={"logo512.png"} alt="Chevrolet"/> */}
+        {/* <img src={"logo512.png"} alt="New balance"/>
+        <img src={"logo512.png"} alt="Strega"/>
+        <img src={"logo512.png"} alt="University of Massachusetts Medical School"/>
+        <img src={"logo512.png"} alt="Boston logan"/> */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
